78701a07a7c9ae7aa212dd8fba52576fbde324bb
Leonardo Arruda <la000476@gmail.com>
2025-10-07 00:12:19 +0000

    Classe Tarefa

---
+++ tarefas/Tarefa.cs
@@ -0,0 +1,12 @@
+public class Tarefa{
+    public Tarefa{
+        
+    }
+
+    public int id {get; set;}
+    public string nome {get; set;}
+    public string descricao {get; set;}
+    public DateTime dataDescricao {get; set;}
+    public int status {get; set;}
+    public DateTime dataExecucao {get;}
+}
